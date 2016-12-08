44ee12cb76e3f6190049e8f97a9dd3b6b9001b7d
s1mpler <stas.dior@gmail.com>
2016-12-08 14:22:33 +0100

    Update output provider

--- src/com/company/utils/OutputProvider.java
+++ src/com/company/utils/OutputProvider.java
@@ -1,7 +1,41 @@
 package com.company.utils;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
+ * Provides functions to manipulate output into the files.
  * Created by e_voe_000 on 12/8/2016.
  */
 public class OutputProvider {
+
+    /**
+     * Method for file initialization.
+     * @param filename Name of the file user wants to create
+     * @param numbers Array of integers that file will contain
+     */
+    public void initFile(String filename, int[] numbers) {
+        try{
+            PrintWriter writer = new PrintWriter(filename, "UTF-8");
+            for (int i = 0; i < numbers.length; i++) {
+                writer.println(numbers[i]);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Clears the content of the file
+     * @param filename Name of the file user wants to clear
+     */
+    public void clearFile(String filename) {
+        try {
+            new PrintWriter(filename).close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
