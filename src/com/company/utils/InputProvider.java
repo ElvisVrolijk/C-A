5e622c2afccb4474b5d8d15e93eb2ca93b76c09c
e_voe_000 <e_voetbal_guy@hotmail.com>
2016-12-08 14:25:51 +0100

    Add gitgnore

--- src/com/company/utils/InputProvider.java
+++ src/com/company/utils/InputProvider.java
@@ -1,15 +1,33 @@
 package com.company.utils;
 
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by e_voe_000 on 12/8/2016.
  */
 public class InputProvider {
 
-    private Random random = new Random();
+    private Scanner reader;
 
-    public void createFile() {
+    public int[] readFile(String fileName, int size) {
+        int[] array = new int[size];
+        try {
+            reader = new Scanner(new FileReader(fileName));
+            int i = 0;
+            while ( reader.hasNextLine()) {
+                array[i] = reader.nextInt();
+                i++;
+            }
 
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
+        return array;
     }
 }
