7cfe440238379f7ebb52dbfd1b264d0f18d49a32
e_voe_000 <e_voetbal_guy@hotmail.com>
2016-12-08 13:58:14 +0100

    Add utils

---
+++ src/com/company/utils/InputProvider.java
@@ -0,0 +1,15 @@
+package com.company.utils;
+
+import java.util.Random;
+
+/**
+ * Created by e_voe_000 on 12/8/2016.
+ */
+public class InputProvider {
+
+    private Random random = new Random();
+
+    public void createFile() {
+
+    }
+}
---
+++ src/com/company/utils/OutputProvider.java
@@ -0,0 +1,7 @@
+package com.company.utils;
+
+/**
+ * Created by e_voe_000 on 12/8/2016.
+ */
+public class OutputProvider {
+}
